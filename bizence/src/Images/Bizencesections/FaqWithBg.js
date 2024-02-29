b438db129071303f3482420e48cf2efeb8220481
LAPTOP-JF87ASUP\Ayush <ayushpatil72680@gmail.com>
2024-03-01 02:25:55 +0530

    faq content

--- bizence/src/Images/Bizencesections/FaqWithBg.js
+++ bizence/src/Images/Bizencesections/FaqWithBg.js
@@ -27,28 +27,36 @@ export function FaqWithBg() {
           }
         >
           <Accordion.Item className={classes.item} value="reset-password">
-            <Accordion.Control>How can I reset my password?</Accordion.Control>
-            <Accordion.Panel style={{color:'white'}} >{placeholder }</Accordion.Panel>
+            <Accordion.Control>Who can be eligible to participate in this event? </Accordion.Control>
+            <Accordion.Panel style={{color:'white'}} >Any person is eligible to participate, as long as they are interested in the world of business and entrepreneurship.</Accordion.Panel>
           </Accordion.Item>
 
           <Accordion.Item className={classes.item} value="another-account">
-            <Accordion.Control>Can I create more that one account?</Accordion.Control>
-            <Accordion.Panel style={{color:'white'}}>{placeholder}</Accordion.Panel>
+            <Accordion.Control>What necessities should I bring? </Accordion.Control>
+            <Accordion.Panel style={{color:'white'}}>Bring your own laptops, your Aadhar Card and college ID card. If your product does not utilise software or tech, bring a prototype working model of your product.</Accordion.Panel>
           </Accordion.Item>
 
           <Accordion.Item className={classes.item} value="newsletter">
-            <Accordion.Control>How can I subscribe to monthly newsletter?</Accordion.Control>
-            <Accordion.Panel style={{color:'white'}}>{placeholder}</Accordion.Panel>
+            <Accordion.Control>What should be the size of a team?</Accordion.Control>
+            <Accordion.Panel style={{color:'white'}}>Each team can consist of 3-4 people</Accordion.Panel>
           </Accordion.Item>
 
           <Accordion.Item className={classes.item} value="credit-card">
-            <Accordion.Control>Do you store credit card information securely?</Accordion.Control>
-            <Accordion.Panel style={{color:'white'}}>{placeholder}</Accordion.Panel>
+            <Accordion.Control>What is the entry fee?</Accordion.Control>
+            <Accordion.Panel style={{color:'white'}}>Entry fees will be 120/- per person</Accordion.Panel>
           </Accordion.Item>
 
           <Accordion.Item className={classes.item} value="payment">
-            <Accordion.Control>What payment systems to you work with?</Accordion.Control>
-            <Accordion.Panel style={{color:'white'}}>{placeholder}</Accordion.Panel>
+            <Accordion.Control>Where will the hackathon take place?</Accordion.Control>
+            <Accordion.Panel style={{color:'white'}}>The hackathon will take place inside the college premises. The ideation process will be conducted in the labs of our college.</Accordion.Panel>
+          </Accordion.Item>
+          <Accordion.Item className={classes.item} value="paymt">
+            <Accordion.Control>Will we be provided with meals? </Accordion.Control>
+            <Accordion.Panel style={{color:'white'}}>Yes, all participants will be provided with 3 full meals and snacks during the event.</Accordion.Panel>
+          </Accordion.Item>
+          <Accordion.Item className={classes.item} value="pay">
+            <Accordion.Control>I have no knowledge or experience when it comes to entrepreneurship, what do I do?</Accordion.Control>
+            <Accordion.Panel style={{color:'white'}}> No problem, if you are not well versed in the world of entrepreneurship, we are conducting a BMC workshop to help you with your ideas and the mentors will provide guidance for the same.</Accordion.Panel>
           </Accordion.Item>
         </Accordion>
       </Container>
